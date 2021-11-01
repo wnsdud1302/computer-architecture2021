38ebff6f50b0ad76550f13c04bfc26fa74cd8cca
wnsdud1302 <wnsdud1302@naver.com>
2021-11-01 18:52:36 +0000

    lab4

--- Lab3/main.c
+++ Lab3/main.c
@@ -3,12 +3,12 @@
 extern void bubblesort(int arr[], int index);
 extern void sort(int arr[], int index);
 
-void swap(int *a, int *b){
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
+//void swap(int *a, int *b){
+// 	int temp;
+// 	temp = *a;
+// 	*a = *b;
+// 	*b = temp;
+// }
 
 // void sort(int v[], int n){
 // 	for(int i = 0; i < n-1; i++)
@@ -19,23 +19,22 @@ void swap(int *a, int *b){
 
 int main()
 {
-	int arr[4] = {7,3,1,2};
+	//int arr[4] = {7,3,1,2};
 	int i;
 		
-	printf("Array before sorting :");
-	for(i = 0; i < sizeof(arr)/sizeof(int); i++)
-		printf(" %d ",arr[i]);
-	printf("\n");
+	// printf("Array before sorting :");
+	// for(i = 0; i < sizeof(arr)/sizeof(int); i++)
+	// 	printf(" %d ",arr[i]);
+	// printf("\n");
 		
-	bubblesort(arr, 4);
+	// bubblesort(arr, 4);
 
-	printf("Array after sorting :");
-	for(i = 0; i < sizeof(arr)/sizeof(int); i++)
-		printf(" %d ",arr[i]);
-	printf("\n");
+	// printf("Array after sorting :");
+	// for(i = 0; i < sizeof(arr)/sizeof(int); i++)
+	// 	printf(" %d ",arr[i]);
+	// printf("\n");
 
-		int arr1[4] = {7,3,1,2};
-	int i;
+	int arr1[4] = {7,3,1,2};
 		
 	printf("Array before sorting :");
 	for(i = 0; i < sizeof(arr1)/sizeof(int); i++)
--- Lab3/sort.s
+++ Lab3/sort.s
@@ -5,19 +5,19 @@
 
 sort:  
         sub sp, sp, #24
-		str	lr, [sp, #20]
+	str lr, [sp, #20]
         str r8, [sp, #16] // n-1-i
-		str	r7, [sp, #12] // ncopy
-		str	r6, [sp, #8] // vcopy
-		str r3, [sp, #4] //j
-		str	r2, [sp, #0] //i
+	str r7, [sp, #12] // ncopy
+	str r6, [sp, #8] // vcopy
+	str r3, [sp, #4] //j
+	str r2, [sp, #0] //i
         
         mov r6 ,r0 // v
         mov r7 ,r1 // n
         sub r1, #1
         mov r2, #0
 
-foritst:
+for1tst:
         cmp r2, r1
         bge exit1
         mov r3, #0
@@ -26,7 +26,7 @@ for2tst:
         sub r8, r1, r2
         cmp r3, r8
         bge exit2
-        add r12 r0, r3 LSL #2
+        add r12, r0, r3 ,LSL #2
         ldr r4, [r12, #0]
         ldr r5, [r12, #4]
         cmp r4, r5
@@ -38,12 +38,13 @@ for2tst:
         bl swap
         ldmia sp!,{r0, r1, r2, r3, r12}
 
-        sub r3, r3 #1
+        add r3, r3, #1
         b for2tst
 
 exit2:
         add r2, r2, #1
         b for1tst
+
 exit1:
         ldr r2, [sp, #0]
         ldr r3, [sp, #4]
